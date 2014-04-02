6bab59247d791e39747a1a89394304d877453f24
Morgan Creekmore <gamingrobot@gmail.com>
2014-04-02 09:25:46 -0600

    Golang piping to a browser

---
+++ main.go
@@ -0,0 +1,71 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"github.com/codegangsta/martini"
+	"github.com/gorilla/websocket"
+	"io"
+	"log"
+	"net/http"
+	"os/exec"
+	"strings"
+)
+
+func main() {
+	m := martini.Classic()
+	m.Get("/ws", func(w http.ResponseWriter, r *http.Request) {
+		ws, err := websocket.Upgrade(w, r, nil, 1024, 1024)
+		if _, ok := err.(websocket.HandshakeError); ok {
+			http.Error(w, "Not a websocket handshake", 400)
+			return
+		} else if err != nil {
+			log.Println(err)
+			return
+		}
+		//spawn qemu
+		cmd := exec.Command("qemu-system-arm", "-M", "versatilepb", "-m", "20M", "-nographic", "-readconfig", "qemu.conf")
+		stdout, err := cmd.StdoutPipe()
+		if err != nil {
+			log.Println(err)
+		}
+		go readLoop(stdout, ws)
+		go cmd.Run()
+		for {
+			_, message, err := ws.ReadMessage()
+			if err != nil {
+				//kill qemu
+				cmd.Process.Kill()
+				log.Println(err)
+				return
+			} else {
+				//send msg to qemu
+				cmd.Stdin = strings.NewReader(string(message))
+				fmt.Println(message)
+			}
+		}
+	})
+	address := "localhost:3000"
+	log.Println("Martini started on", address)
+	log.Fatal(http.ListenAndServe(address, m))
+}
+
+func readLoop(output io.Reader, ws *websocket.Conn) {
+	r := bufio.NewReader(output)
+	for {
+		str, err := r.ReadString('\n')
+
+		if err == io.EOF {
+			return
+		}
+		if err != nil {
+			log.Println("Read Line Error:", err)
+			continue
+		}
+		if len(str) == 0 {
+			continue
+		}
+		//fmt.Print(str)
+		ws.WriteMessage(websocket.TextMessage, []byte(str))
+	}
+}
